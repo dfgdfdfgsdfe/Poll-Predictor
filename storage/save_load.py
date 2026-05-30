efe37eafbda58d9039d2b119679edacf7ebaa34e
dfgdfdfgsdfe <yiheewoo04@gmail.com>
2026-05-30 22:06:54 +0900

    Update save_load.py

--- storage/save_load.py
+++ storage/save_load.py
@@ -1,1 +1,55 @@
+from pathlib import Path
+import json
 
+PROJECTS_DIR = Path(
+    "projects"
+)
+
+
+def save_json(
+    project_id,
+    filename,
+    data
+):
+
+    path = (
+        PROJECTS_DIR
+        / project_id
+        / filename
+    )
+
+    with open(
+        path,
+        "w",
+        encoding="utf-8"
+    ) as f:
+
+        json.dump(
+            data,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
+
+
+def load_json(
+    project_id,
+    filename
+):
+
+    path = (
+        PROJECTS_DIR
+        / project_id
+        / filename
+    )
+
+    if not path.exists():
+        return None
+
+    with open(
+        path,
+        "r",
+        encoding="utf-8"
+    ) as f:
+
+        return json.load(f)
