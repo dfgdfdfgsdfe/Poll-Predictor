b86378657ebc7d2b933fe9915bae2f61c76ffef3
dfgdfdfgsdfe <yiheewoo04@gmail.com>
2026-05-30 22:05:59 +0900

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,1 +1,36 @@
+from dataclasses import dataclass
+from typing import Dict
 
+
+@dataclass
+class ElectionState:
+
+    values: Dict[str, float]
+
+    def total(self):
+
+        return sum(
+            self.values.values()
+        )
+
+    def normalize(self):
+
+        total = self.total()
+
+        if total <= 0:
+            return
+
+        for k in self.values:
+
+            self.values[k] = (
+                self.values[k]
+                /
+                total
+            ) * 100
+
+    def get(self, key):
+
+        return self.values.get(
+            key,
+            0
+        )
