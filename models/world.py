4315e1d67189567b277f2f0dfffdc5362b20d91b
dfgdfdfgsdfe <yiheewoo04@gmail.com>
2026-05-30 22:06:13 +0900

    Update world.py

--- models/world.py
+++ models/world.py
@@ -1,1 +1,14 @@
+from dataclasses import dataclass
+from typing import Dict
 
+
+@dataclass
+class World:
+
+    world_id: int
+
+    final_supports: Dict[str, float]
+
+    winner: str
+
+    probability: float = 0.0
